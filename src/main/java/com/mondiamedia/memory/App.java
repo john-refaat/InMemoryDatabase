a8a4a2122add936c7a2e405da41cef3abc625b2f
John Refaat <john.refaat88@gmail.com>
2016-09-23 23:16:24 +0200

    Initial Commit

---
+++ src/main/java/com/mondiamedia/memory/App.java
@@ -0,0 +1,13 @@
+package com.mondiamedia.memory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
